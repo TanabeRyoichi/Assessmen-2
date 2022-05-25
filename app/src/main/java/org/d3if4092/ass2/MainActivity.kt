b1b9f4085525d1dfd727238233a699fdd419e53e
TanabeRyoichi <Tanaberyoichi20107@gmail.com>
2022-05-25 22:35:58 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if4092/ass2/MainActivity.kt
@@ -0,0 +1,43 @@
+package org.d3if4092.ass2
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+            val navController = findNavController(R.id.fragmentContainerView)
+            val  appConfiguration = AppBarConfiguration(setOf(R.id.konversiFragment, R.id.hasilFragment))
+            setupActionBarWithNavController(navController, appConfiguration)
+
+            bottomNavigationView.setupWithNavController(navController)
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/org/d3if4092/ass2/ui/KonversiFragment.kt
@@ -0,0 +1,71 @@
+package org.d3if4092.ass2.ui
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import org.d3if4092.ass2.R
+import org.d3if4092.ass2.databinding.FragmentKonversiBinding
+
+class KonversiFragment : Fragment() {
+
+
+
+    private lateinit var binding: FragmentKonversiBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreate(savedInstanceState)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.button.setOnClickListener { konversi() }
+        binding.button3.setOnClickListener { reset() }
+    }
+
+
+    @SuppressLint("StringFormatMatches")
+    private fun konversi() {
+
+
+        val jumlah = binding.namaInp.text.toString()
+        if (TextUtils.isEmpty(jumlah)) {
+            Toast.makeText(context, R.string.jumlah_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val hasil = jumlah.toFloat() / 132
+
+        binding.jumlahTextView.text = getString(R.string.jumlah_x, hasil)
+
+    }
+
+    private fun reset() {
+        binding.namaInp.setText("")
+        binding.jumlahTextView.text = ""
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
